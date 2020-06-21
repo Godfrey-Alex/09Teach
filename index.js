164d665b3f4eb640e95a80bf4d9c9bebaabf34c1
Alex <god18007@byui.edu>
2020-06-20 23:14:18 -0600

    adding stuff

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.set('view engine', 'ejs');
 
 // set up a rule that says requests to "/math" should be handled by the
 // handleMath function below
-app.get('/', (req, res) => res.send("Welcome to the Root"))
+app.get('/', (req, res) => res.send("Welcome to the root!"))
 app.get('/form', (req, res) => res.send(form.html))
-app.get('/math', handleMath);
+app.get('/mail', handleMail);
 
 // start the server listening
 app.listen(port, function() {
@@ -27,36 +27,64 @@ app.listen(port, function() {
  * they are listed here.
  **********************************************************************/
 
-function handleMath(request, response) {
-    console.log("Starting the math equation!...")
-	const operation = request.query.operation;
-	const operand1 = Number(request.query.operand1);
-	const operand2 = Number(request.query.operand2);
+function handleMail(request, response) {
+    console.log("Starting the mail calculation...")
+	const mailOperation = request.query.mailType;
+	console.log("Mail Operations: "+mailOperation);
+	//const pounds = Number(request.query.pounds);
+	//console.log("Mail Pounds: "+pounds);
+	const ounces = Number(request.query.ounces);
+	console.log("Mail Ounces: "+ounces);
 
 	// TODO: Here we should check to make sure we have all the correct parameters
 
-	computeOperation(response, operation, operand1, operand2);
+	computeCost(response, mailOperation, ounces);
 }
 
-function computeOperation(response, op, left, right) {
-	op = op.toLowerCase();
+function computeCost(response, op, oz) {
+	//op = op.toLowerCase();
 
-	let result = 0;
+	let cost = 0;
 
-	if (op == "add") {
-		result = left + right;
-	} else if (op == "subtract") {
-		result = left - right;		
-	} else if (op == "multiply") {
-		result = left * right;
-	} else if (op == "divide") {
-		result = left / right;
+	if (op == "Letters (Stamped)") {
+		if(oz==1){
+			cost=.55;
+		}else if(oz==2){
+			cost=.7;
+		}else if(oz==3){
+			cost=.85;
+		}else if(oz>=3.5){
+			cost=1;
+		}
+	} else if (op == "Letters (Metered)") {
+		if(oz==1){
+			cost=.50;
+		}else if(oz==2){
+			cost=.65;
+		}else if(oz==3){
+			cost=.80;
+		}else if(oz>=3.5){
+			cost=.95;
+		}		
+	} else if (op == "Large Envelopes (Flats)") {
+		cost = 1 +(oz-1)*.2;
+	} else if (op == "First-Class Package Service-Retail") {
+		if(oz<=4){
+			cost=3.8;
+		}else if(oz>4 && oz<=8){
+			cost=4.6;
+		}else if(oz>8 && oz<=12){
+			cost=5.3;
+		}else if(oz>12){
+			cost=5.9;
+		}	
 	} else {
 		
 	}
+	console.log("Cost: " + cost);
 
 	// Set up a JSON object of the values we want to pass along to the EJS result page
-	const params = {operation: op, left: left, right: right, result: result};
+	const params = {operation: op, weight: oz, cost: cost};
 
 	// Render the response, using the EJS page "result.ejs" in the pages directory
 	// Makes sure to pass it the parameters we need.
